cb096919936de61fb9af29acdac20196d5590cfe
Sahildholpuria <dholpuria1999@gmail.com>
2024-06-04 15:17:02 +0530

    Verify logo test cases added

--- src/test/java/AvonLogin.java
+++ src/test/java/AvonLogin.java
@@ -3,8 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
@@ -32,7 +32,7 @@ public class AvonLogin {
     public void verifyTitle()
     {
         String actualTitle = driver.getTitle();
-        String expectedTitle = "Login";
+        String expectedTitle = "Logn";
 
         Assert.assertEquals(actualTitle,expectedTitle);
     }
@@ -52,6 +52,16 @@ public class AvonLogin {
     {
         boolean flag = driver.findElement(By.xpath("//*[@id=\"main\"]/webruntime-app/lwr-router-container/webruntime-inner-app/dxp_data_provider-user-record-data-provider/dxp_data_provider-data-proxy/dxp_data_provider-user-data-provider/dxp_data_provider-data-proxy/b2c_lite_commerce-external-layout/webruntime-router-container/dxp_data_provider-user-record-data-provider/dxp_data_provider-data-proxy/dxp_data_provider-user-data-provider/dxp_data_provider-data-proxy/community_layout-slds-flexible-layout/div/community_layout-section/div[3]/community_layout-column/div/dxp_content_layout-site-logo/a/div")).isDisplayed();
         Assert.assertTrue(flag);
+    }
+
+    @Test
+    public void verifyProducts() throws InterruptedException {
+        Thread.sleep(4000);
+        driver.findElement(By.xpath("//*[@id=\"input-26\"]")).sendKeys(emailAddress);
+        driver.findElement(By.xpath("//*[@id=\"input-28\"]")).sendKeys(Password);
+        driver.findElement(By.xpath("//*[@id=\"main\"]/webruntime-app/lwr-router-container/webruntime-inner-app/dxp_data_provider-user-record-data-provider/dxp_data_provider-data-proxy/dxp_data_provider-user-data-provider/dxp_data_provider-data-proxy/b2c_lite_commerce-external-layout/webruntime-router-container/dxp_data_provider-user-record-data-provider/dxp_data_provider-data-proxy/dxp_data_provider-user-data-provider/dxp_data_provider-data-proxy/community_layout-slds-flexible-layout/div/community_layout-section/div[3]/community_layout-column/div/community_login-login-form/div/div[1]/div/button")).click();
+
+        driver.findElement(By.xpath("//*[@id=\"main\"]/webruntime-app/lwr-router-container/webruntime-inner-app/dxp_data_provider-user-record-data-provider/dxp_data_provider-data-proxy/dxp_data_provider-user-data-provider/dxp_data_provider-data-proxy/commerce-layout-site/div[1]/webruntime-component-container[1]/commerce_builder-layout-header/commerce-layout-header/header/div/div/div[2]/div/commerce_builder-drilldown-navigation/commerce-drilldown-navigation/commerce-drilldown-navigation-bar/nav/ul/li[2]/button")).click();
     }
 
     @AfterMethod
